af4209be3280ae6ed06676b148a4fa74abe969c8
Thandekaportiap <thandekaportiap05@gmail.com>
2024-09-17 16:12:44 +0200

    register useState

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaRegUser } from "react-icons/fa6";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { MdOutlineAttachEmail } from "react-icons/md";
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email:'',
+    password: '',
+    confirmpassword: ''
+  });
+  const [errors, setErrors] = useState({});
+  const [valid, setValid] = useState(true)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    // console.log(formData)
+    let isvalid = true;
+    let validationErrors = {}
+    if(formData.username === "" || formData.username === null) {
+      isvalid = false;
+      validationErrors.username = "Username is required"
+    }
+    if(formData.email === "" || formData.email === null) {
+      isvalid = false;
+      validationErrors.username = "Email is required"
+    } else if(!/\S+@\S+\.\S+/.test(formData.email)) {
+      isvalid = false;
+      validationErrors.email = "Email is not valid"
+    } 
+  }
+
+
   return (
     <div className="flex items-center justify-center h-screen bg-whitesmoke">
       <div className="bg-white p-8 rounded-lg shadow-lg border-2 border-[#006D5B] lg:w-96 ssm:w-64"> 
         <h2 className="text-2xl font-semibold mb-6 text-center">Register Page</h2>
         <p className="text-gray-600 mb-8 text-center">Create your new account </p>
+        <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label htmlFor="username" className="block text-gray-700">Username</label>
           <div className="flex items-center border-b-2 border-[#006D5B]">
@@ -17,6 +46,7 @@ const RegisterPage = () => {
               type="text" 
               id="username" 
               className="w-full py-2 outline-none" 
+              onChange={(event) => setFormData({...formData, username: event.target.value})}
               style={{ borderColor: '#006D5B' }} 
             />
           </div>
@@ -29,6 +59,7 @@ const RegisterPage = () => {
               type="email" 
               id="email" 
               className="w-full py-2 outline-none" 
+              onChange={(event) => setFormData({...formData, email: event.target.value})}
               style={{ borderColor: '#006D5B' }} 
             />
           </div>
@@ -41,6 +72,7 @@ const RegisterPage = () => {
               type="password" 
               id="password" 
               className="w-full py-2 outline-none" 
+              onChange={(event) => setFormData({...formData, password: event.target.value})}
               style={{ borderColor: '#006D5B' }} 
             />
           </div>
@@ -52,7 +84,8 @@ const RegisterPage = () => {
             <input 
               type="password" 
               id="confirmPassword" 
-              className="w-full py-2 outline-none" 
+              className="w-full py-2 outline-none"
+              onChange={(event) => setFormData({...formData, confirmpassword: event.target.value})} 
               style={{ borderColor: '#006D5B' }} 
             />
           </div>
@@ -63,6 +96,7 @@ const RegisterPage = () => {
         >
           Register
         </button>
+        </form>
       </div>
     </div>
   );
